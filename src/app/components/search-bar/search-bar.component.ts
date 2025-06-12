e8ccaeb4a3ddee0abcf89473a15d514c0e259a4a
xiomi94 <xiomarajimvel94@gmail.com>
2025-06-12 10:44:03 +0100

    Add styles to search-bar-component

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,9 +1,14 @@
 import {ChangeDetectionStrategy, Component, output} from '@angular/core';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
+  imports: [
+    NzInputModule,
+    NzIconModule 
+  ],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
